763406cfb97a4e3f2870c633e4a893acfa7fc753
iqbaldota <107950105+iqbaldota@users.noreply.github.com>
2023-01-11 17:09:52 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                lat: 101.58256574232948,
-                lng: 3.082294519837642,
+                lat: 3.082294519837642,
+                lng: 101.58256574232948,
             },
         },
     ];
@@ -22,19 +22,19 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '1 1 1',
+        scale: '0.5 0.5 0.5', 
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
     {
         url: './assets/articuno/scene.gltf',
-        scale: '1 1 1',
+        scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '1 1 1',
+        scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
